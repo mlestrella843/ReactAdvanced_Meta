48da6db35fb866c64496e9d58162beb8ef644309
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-12-10 15:59:59 -0700

    Updating an object using state

--- react_advanced/src/components/hooks.jsx
+++ react_advanced/src/components/hooks.jsx
@@ -2,21 +2,20 @@ import { useState } from "react";
 
 const GreetClients = () => {
 
-    const [greeting, setGreeting] = useState({ greet: "Hello guys!"});
+    const [greeting, setGreeting] = useState({ user: "Maria", greet:"Hello guys!"});
     console.log(greeting, setGreeting);
 
     function updatedGreeting (){
-        setGreeting({greet: "Hello Maria Estrella!"});
+        const newGreeting = {...greeting};
+        newGreeting.greet = "Hi, how is going?";
+        setGreeting(newGreeting);
     }
-
     return(
         <div>  
             <button className="bg-blue-300 text-white rounded border-2 border-blue-600 px-3 py-2" onClick={updatedGreeting}>Update your Greetings</button>
             <h1>{greeting.greet}</h1>
+            <h2>{greeting.user}</h2>
         </div>
-
     );
-
 };
-
 export {GreetClients};
